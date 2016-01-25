f8a45844ddfcc21a6cb1dc677b7ee33ec9d311b2
azu <azuciao@gmail.com>
2016-01-25 12:30:12 +0900

    fix(lib): support the case that not found current version in npm
    package.json : 1.0.1
    npm latest: 1.0.0
    
    `getVersionsAsync` return 1.0.0
    
    fix #1

--- src/npm-relative-version.js
+++ src/npm-relative-version.js
@@ -2,12 +2,12 @@
 "use strict";
 import npm from "npm";
 import semver from "semver";
-export function npmLoad() {
+export function npmLoad(options = {}) {
     if (npm.config.loaded) {
         return Promise.resolve(npm);
     }
     return new Promise((resolve, reject) => {
-        npm.load((error, npm) => {
+        npm.load(options, (error, npm) => {
             if (error) {
                 reject(error);
             } else {
@@ -34,12 +34,28 @@ export function getVersionsAsync(pkg) {
     var version = getVersion(pkg);
     return npmLoad().then(npm => {
         return new Promise((resolve, reject) => {
-            npm.commands.view([`${pkgName}@${version}`, "versions"], true, (error, result) => {
+            npm.commands.view([`${pkgName}`, "versions"], true, (error, result) => {
                 if (error) {
                     return reject(error);
                 }
+                /*
+                 result is following object:
+                {
+                '2.27.0':
+                     { versions:
+                         [ '0.8.3',
+                         '0.9.0',
+                         '0.9.1',
+                         ...
+                         '2.26.0',
+                         '2.27.0' ]
+                     }
+                 }
+                 */
+                var remoteVersion = Object.keys(result)[0];
+                var remoteVersions = result[remoteVersion].versions;
                 // pick up [version]"versions"
-                resolve(result[version]["versions"]);
+                resolve(remoteVersions);
             });
         });
     });
@@ -73,6 +89,10 @@ export function getRelativeVersion(pkg, options = {}) {
     var currentVersion = getVersion(pkg);
     return getVersionsAsync(pkg).then(versions => {
         let indexOfCurrent = versions.indexOf(currentVersion);
+        // if not found current version in npm, return `latest` version
+        if(indexOfCurrent == -1) {
+            return versions[versions.length - 1];
+        }
         if (isLtr) {
             return findVersionMatchDiff(currentVersion, versions.slice(0, indexOfCurrent), type);
         } else {
